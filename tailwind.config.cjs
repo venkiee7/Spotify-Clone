315d612222e4a09a315a48f7b8cfaea98f9eff05
Vyankatesh Jaju <jajuvyankatesh2@gmail.com>
2022-10-29 21:14:46 +0530

    Initial Commit-Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    colors: {
+      'green' : "#1db954",
+      'black-base' : "#121212",
+      'black-primary' : "#191414",
+      'black-secondary' : "#171818",
+      'white' : "#ffffff",
+      'primary' : "#FFFFFF",
+      'light-black' : "#282828",
+      'secondary' : "#b3b3b3",
+      'gray' : "#535353"
+    },
+    gridTemplateColumns: {
+      'auto-fill-cards': 'repeat(auto-fill, minmax(200px,1fr))'
+    }
+  }
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+}
