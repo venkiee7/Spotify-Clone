24e5f18a4a512828284950ae4bcafde938deffb7
Vyankatesh Jaju <jajuvyankatesh2@gmail.com>
2022-11-14 21:06:34 +0530

    My playlists endpoints

--- src/common.js
+++ src/common.js
@@ -1,13 +1,15 @@
 export const ACCESS_TOKEN = "ACCESS_TOKEN";
 export const TOKEN_TYPE = "TOKEN_TYPE";
 export const EXPIRES_IN = "EXPIRES_IN";
+export const LOADED_TRACKS = "LOADED_TRACKS"
 const APP_URL = import.meta.env.VITE_APP_URL;
 
 export const ENDPOINT = {
     userInfo: "me",
     featuredPlaylist: "browse/featured-playlists?limit=5",
     toplists: "browse/categories/toplists/playlists?limit=10",
-    playlist: "playlists"
+    playlist: "playlists",
+    userPlaylist: "me/playlists"
 }
 
 export const logout = () => {
@@ -16,6 +18,10 @@ export const logout = () => {
     localStorage.removeItem(TOKEN_TYPE);
     window.location.href = APP_URL;
 }
+
+export const setItemsInLocalStorage = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+
+export const getItemsFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export const SECTIONTYPE = {
     DASHBOARD : "DASHBOARD",
